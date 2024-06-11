06a7fc022bead8cda3772d7abbd58c16c4bafc91
Mandar Dhamale <mandardhamale@gmail.com>
2024-06-11 19:36:56 +0530

    S12L20 - Blocking Queue in multithreading continues

--- src/main/java/multithreading/blockingqueue/Main.java
+++ src/main/java/multithreading/blockingqueue/Main.java
@@ -14,7 +14,7 @@ class Producer implements  Runnable{
     public void run() {
         while (true){
             try {
-                Thread.sleep(1000);
+                Thread.sleep(500);
                 queue.put(Main.counter);
                 System.out.println(Main.counter + " added in queue");
                 Main.counter++;
@@ -25,21 +25,52 @@ class Producer implements  Runnable{
     }
 }
 
+class Consumer implements  Runnable{
+    private ArrayBlockingQueue<Integer> queue;
+
+    public Consumer(ArrayBlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            try {
+                Thread.sleep(500);
+                queue.take();
+                System.out.println(Main.counter + " removed from queue");
+                Main.counter--;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+
 public class Main {
     public static int counter = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
         Thread thread1 = new Thread(new Producer(queue));
         thread1.start();
 
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Test runnable interfrace");
-            }
-        });
+        Thread thread2 = new Thread(new Consumer(queue));
         thread2.start();
+
+//        ArrayBlockingQueue<Integer> queue2 = new ArrayBlockingQueue<>(10);
+//        Thread thread2 = new Thread(new Producer(queue2));
+//        thread2.start();
+
+//  testing a runnable interface
+//        Thread thread2 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Test runnable interfrace");
+//            }
+//        });
+//        thread2.start();
 
     }
 }
