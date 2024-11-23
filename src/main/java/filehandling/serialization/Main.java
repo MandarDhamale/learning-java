78bb8838df849519687f87de804d3f62f982b651
Mandar Dhamale <mandardhamale@gmail.com>
2024-11-23 20:01:39 +0530

    Debugging

--- src/main/java/filehandling/serialization/Main.java
+++ src/main/java/filehandling/serialization/Main.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class Main {
+
+    public static int x = 10;
+
     public static void main(String[] args) {
         Vehicle bike = new Vehicle("Bike", "1234");
         Vehicle car = new Vehicle("Car", "3241");
